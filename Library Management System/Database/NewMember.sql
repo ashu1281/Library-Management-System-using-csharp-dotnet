c6e0fc52519b77c791b7b12f36ca33110f34d9ec
ashu1281 <gaiwadashish628@gmail.com>
2023-03-28 01:13:07 +0530

    NewMember Table qurery

---
+++ Library Management System/Database/NewMember.sql
@@ -0,0 +1,13 @@
+﻿create table NewMember(
+memId int NOT NULL IDENTITY(1,1) primary key,
+mName varchar(250) not null,
+mContact bigint not null,
+mEmail varchar(250) not null,
+mState varchar(250) not null,
+mCity varchar(250) not null,
+mPinCode bigint not null
+)
+
+
+--mName, mContact, mEmail, mState, mCity, mPinCode
+select * from NewMember
