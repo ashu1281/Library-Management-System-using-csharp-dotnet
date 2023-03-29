9939072cbfb2bb60468e88ce8a25c664b571eee7
ashu1281 <gaiwadashish628@gmail.com>
2023-03-30 00:50:00 +0530

    IssueBook form

---
+++ Library Management System/IssueBook.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Library_Management_System
+{
+    public partial class IssueBook : Form
+    {
+        public IssueBook()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearchEnrollNo_Click(object sender, EventArgs e)
+        {
+            if (txtEnroll.Text != null)
+            {
+                String eid = txtEnroll.Text;
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=LibraryManagement;Integrated Security=True;Pooling=False";
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+
+                cmd.CommandText = "select * from NewMember where EnrollID='" + eid + "'";
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                if (ds.Tables[0].Rows.Count != 0)
+                {
+                    txtName.Text = ds.Tables[0].Rows[0][2].ToString();
+                    txtContact.Text = ds.Tables[0].Rows[0][3].ToString();
+                    txtEmail.Text = ds.Tables[0].Rows[0][4].ToString();
+                }
+            }
+        }
+
+        private void IssueBook_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=LibraryManagement;Integrated Security=True;Pooling=False";
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            conn.Open();
+
+            string sqlCommandText = string.Empty;
+            sqlCommandText = "select bName from NewBook";
+
+            cmd = new SqlCommand(sqlCommandText, conn);
+            SqlDataReader Sdr = cmd.ExecuteReader();
+
+            while(Sdr.Read()) 
+            { 
+                for(int i=0; i < Sdr.FieldCount; i++)
+                {
+                    comboBoxBooks.Items.Add(Sdr.GetString(i));
+                }
+            }
+
+            Sdr.Close();
+            conn.Close();
+
+        }
+
+        private void txtEnroll_TextChanged(object sender, EventArgs e)
+        {
+            if(txtEnroll.Text == "")
+            {
+                txtName.Clear();
+                txtContact.Clear();
+                txtEmail.Clear();
+                comboBoxBooks.SelectedItem = null;
+
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            txtEnroll.Clear();
+        }
+    }
+}
